8fb1e93990630b4ffc96209ae25c659f540cefcf
mikaijun <mj.tempe443@gmail.com>
2023-10-08 07:21:03 +0000

    main.goを修正

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
+func init() {
+	// 環境変数を取得する
+	host := os.Getenv("HOST")
+	database := os.Getenv("DATABASE")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	// データベースに接続する
+	_, err := sql.Open("postgres", fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, database))
+	if err != nil {
+		panic(err)
+	}
+
+	// 後でマイグレーションのモジュールを作成する
+
+}
+
 func main() {
-	fmt.Printf("Hello, world!")
+
+	// 後でAPIエンドポイントのモジュールを作成する
+
+	fmt.Println("Server is running on port 8080...")
+	http.ListenAndServe(":8080", nil)
 }
